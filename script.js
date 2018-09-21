832a21b58e7ac160de6992e65e8d623b2b90f914
guirocato <gui@boldin.com.br>
2018-09-21 10:37:56 -0300

    xssxa

---
+++ script.js
@@ -0,0 +1,62 @@
+function myFunction(y) {
+    var x = document.getElementById("col-md-4");
+    if (x.style.display === "block") {
+    	y.classList.toggle("change");
+        x.style.display = "none";
+    } else {
+    	y.classList.toggle("change");
+        x.style.display = "block";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  let db = connect('https://codes-grupo2-p1.firebaseio.com/')
+
+  
+  let params = extract()
+
+  let path = '/' + params['category'] + '/projetos/' + params['projeto']
+
+  db.download(path, function(data) {
+   
+    replace('body', {
+        'name': data['name'],
+        'items': data['items'],
+    })
+    replace('title', {
+        'name': data['name'],
+    })
+  })
+})
+
+////Carrossel
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
